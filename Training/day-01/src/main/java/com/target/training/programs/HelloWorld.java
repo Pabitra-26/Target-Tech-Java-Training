35cde66f413b11d9d9309eb8dcdfd667e23f87a9
Pabitra <pabitrapriyadarshinijena@gmail.com>
2021-10-11 22:21:33 +0530

    Made changes in day-01 folder Added the word Corporartion in the print statement

--- Training/day-01/src/main/java/com/target/training/programs/HelloWorld.java
+++ Training/day-01/src/main/java/com/target/training/programs/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
     }
 
     static void welcome(){
-        log.debug("Welcome to Java Programming at Target!");
+        log.debug("Welcome to Java Programming at Target Corporation!");
     }
 
     public static void main(String[] args) {
